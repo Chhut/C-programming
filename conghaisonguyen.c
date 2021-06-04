22e40c378337de610d428c8c50ca2c455f35d79b
Chhut <85237497+Chhut@users.noreply.github.com>
2021-06-04 15:26:59 +0700

    Add files via upload

---
+++ conghaisonguyen.c
@@ -0,0 +1,13 @@
+//chuong trinh cong hai so nguyen
+#include<stdio.h>
+void main(){
+	//khai bao bien
+	int num1,num2,sum;
+	//nhap du lieu vao,luu o cac bien
+	puts("Nhap hai so nguyen");
+	scanf("%d %d",&num1,&num2);
+	//cong hai so nguyen
+	sum=num1+num2;
+	//hien thi ket qua tren man hinh
+	printf("%d+%d=%d \n",num1,num2,sum);
+}
